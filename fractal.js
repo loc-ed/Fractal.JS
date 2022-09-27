d1900b8b70f5b308627aa654c822c25dd8befbef
loc-ed <cherrifredericks@gmail.com>
2022-09-27 12:31:37 +0200

    can select between different julia presets

--- fractal.js
+++ fractal.js
@@ -12,26 +12,33 @@ window.addEventListener('load', function() {
             this.canvasHeight = canvasHeight;
             this.max_iterations = 100;
             this.pattern = "mandelbrot";
+            this.cy = 0;
+            this.cx = 0;
+            this.preset = 1
+            
         }
 
         drawMandelbrot(context) {
             var imageData = context.createImageData(this.canvasWidth, this.canvasHeight);
             var iData = imageData.data;
 
+            // var x0 =  (Math.random() * 4) - 2;
+            // var y0 =  (Math.random() * 4) - 2;
+            
             for (var i = 0; i < this.canvasHeight; i++) {
                 for (var j = 0; j < this.canvasWidth; j++) {
 
                     //limit the axis 
-                    var x0 = -2.0 + j * 3.0 /this.canvasWidth         //(-2,1)
-                    var y0 = -1.0 + i * 2.0 / this.canvasHeight       //(-1,1)
+                    this.cx = -2.0 + j * 3.0 /this.canvasWidth         //(-2,1)
+                    this.cy = -1.0 + i * 2.0 / this.canvasHeight       //(-1,1)
 
                     var x = 0;
                     var y = 0;
                     var iteration = 0
 
                     while ((x * x + y * y < 4) && (iteration < this.max_iterations)) {
-                        var x_n = x * x - y * y + x0;
-                        var y_n = 2 * x * y + y0;
+                        var x_n = x * x - y * y + this.cx;
+                        var y_n = 2 * x * y + this.cy;
                         x = x_n;
                         y = y_n;
                         iteration++;
@@ -53,9 +60,36 @@ window.addEventListener('load', function() {
             var imageData = context.createImageData(this.canvasWidth, this.canvasHeight);
             var iData = imageData.data;
 
-            var x0 = -0.4;
-            var y0 = -0.6;
-
+            // Julia presets and their corresponding locations in the Mandelbrot set
+            if (this.preset == 1) {
+                this.cx = -0.4;
+                this.cy = -0.6;
+            }
+            if (this.preset == 2) {
+                this.cx = -0.79;
+                this.cy = 0.15;
+            }
+            if (this.preset == 3) {
+                this.cx = -0.162;
+                this.cy = 1.04;
+            }
+            if (this.preset == 4) {
+                this.cx = 0.3;
+                this.cy = -0.01;
+            }
+            if (this.preset == 5) {
+                this.cx = -1.476;
+                this.cy = 0;
+            }
+            if (this.preset == 6) {
+                this.cx = -0.12;
+                this.cy = -0.77;
+            }
+            if (this.preset == 7) {
+                this.cx = 0.28;
+                this.cy = 0.008;
+            }
+           
             for (var i = 0; i < this.canvasHeight; i++) {
                 for (var j = 0; j < this.canvasWidth; j++) {
             
@@ -66,8 +100,8 @@ window.addEventListener('load', function() {
                     var iteration = 0; 
                     
                     while ((x * x + y * y < 4) && (iteration < this.max_iterations)) {
-                        var x_n = x * x - y * y + x0;
-                        var y_n = 2 * x * y + y0;
+                        var x_n = x * x - y * y + this.cx;
+                        var y_n = 2 * x * y + this.cy;
                         x = x_n;
                         y = y_n;
                         iteration++;
@@ -97,6 +131,10 @@ window.addEventListener('load', function() {
         setPattern(pattern) {
             this.pattern = pattern
         }
+
+        selectPreset(preset) {
+            this.preset = preset
+        }
     }
 
     const fractal = new Fractal(canvas.width, canvas.height);
@@ -114,9 +152,13 @@ window.addEventListener('load', function() {
 
     });
     const julia = document.getElementById('julia');
+    const juliaPreset = document.getElementById('julia-select')
     julia.addEventListener('click', function() {
         fractal.setPattern("julia")
-
+        //dropdown menu to select preset
+        juliaPreset.addEventListener('change', function() {
+            fractal.selectPreset(juliaPreset.value)
+        });
     });
 
 });
